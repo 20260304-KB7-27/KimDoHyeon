cddc4d11f861777c4b93cc37ea5dcb332f969b8a
dodo5517 <kh9015548@gmail.com>
2026-07-02 16:52:23 +0900

    backend - 로그인,로그아웃

--- 08_Spring_Vue/backend/src/main/java/org/scoula/config/ServletConfig.java
+++ 08_Spring_Vue/backend/src/main/java/org/scoula/config/ServletConfig.java
@@ -5,9 +5,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
-import org.springframework.web.servlet.config.annotation.*;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.view.JstlView;
         "org.scoula.controller",
         "org.scoula.exception",
         "org.scoula.board.controller",
+        "org.scoula.member.controller",
 })
 public class ServletConfig implements WebMvcConfigurer {
 
